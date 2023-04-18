d9060ab3f6fc61b3d61f78df3d9257750e2964ca
informatics411 <informatics411@gmail.com>
2023-04-18 17:01:18 -0400

    Guid version 1

--- VeniceArtShow.Data/ApplicationDbContext.cs
+++ VeniceArtShow.Data/ApplicationDbContext.cs
@@ -13,4 +13,16 @@ public class ApplicationDbContext : DbContext
     public DbSet<OrderEntity> Orders { get; set;}
 
     public DbSet<ProductEntity> Products { get; set; }
+
+    public DbSet<MediaEntity> Media { get; set; }
+
+    public DbSet<UserEntity> Users { get; set; }
+
+    // protected override void OnModelCreating(ModelBuilder modelBuilder)
+    // {
+    //     modelBuilder.Entity<<MediaEntity>()
+    //     .HasOne(n => n.Username)
+    //     .WithMany(p => p.Media)
+    //     .HasForeignKey(n => n.ArtistId);
+    // }
 }
