99b4aed5d527bb8fdfb2950bbfe95708f4b9733c
informatics411 <informatics411@gmail.com>
2023-04-19 09:18:09 -0400

    sofar ish

---
+++ VeniceArtShow.Models/OrderListWork.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+    
+    //Created, modified w/ EN 17.01 in mind
+    public class OrderListWork
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public  DateTimeOffset CreatedUtc { get; set; }
+    }
--- VeniceArtShow.Services/Order/OrderService.cs
+++ VeniceArtShow.Services/Order/OrderService.cs
@@ -8,14 +8,34 @@ using System.Security.Claims;
 public class OrderService : IOrderService
     {
         private readonly int _userId;
-        public OrderService(IHttpContextAccessor httpContextAccessor)
+        private readonly ApplicationDbContext _dbContext;
+        public OrderService(IHttpContextAccessor httpContextAccessor, ApplicationDbContext dbContext)
         {
-
             var userClaims = httpContextAccessor.HttpContext.User.Identity as ClaimsIdentity;
             var value = userClaims.FindFirst("Id")?.Value;
             var validId = int.TryParse(value, out _userId);
             if (!validId)
                 throw new Exception("Attempted to create an Order without a valid User."
                 );
+                _dbContext = dbContext;
         }
+    public IEnumerable<OrderListWork> GetAllOrdersAsync()
+    {
+
     }
+
+    async Task<IEnumerable<OrderListWork>> IOrderService.GetAllOrdersAsync()
+    {
+        var orders = await _dbContext.Orders
+        .Where(entity => entity.ArtistID == _userId)
+        .Select(UserEntity => new OrderListWork
+        {
+            Id = entity.Id,
+            Title = entity.Title,
+            CreatedUtc = entity.CreatedUtc
+        })
+        .ToListAsync();
+        return orders;
+
+    }
+}
